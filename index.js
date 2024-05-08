6481575ad7fa5bd0f68fea6fc97e6db0d27120ef
Matt Rouse <mrsteamos@gmail.com>
2024-05-08 22:07:06 +0100

    Change options

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ async function requestDevice() {
     }],
     // optionalServices: ["heart_rate"],
   };
-  device = await navigator.bluetooth.requestDevice({ acceptAllDevices: true, optionalServices: [RXTX_SERVICE] });
+  device = await navigator.bluetooth.requestDevice({optionalServices: ["0000dfb0-0000-1000-8000-00805f9b34fb"], acceptAllDevices: true});
   device.addEventListener("gattserverdisconnected", connectDevice);
 }
 
